758dbfcfb80605194311894380efe9ef0273b81e
DSM6 <fatec-dsm6@academico.intra>
2025-05-16 19:20:46 -0300

    Initial Commit

---
+++ src/main/java/br/com/matsumura/TDD_Software/TddSoftwareApplication.java
@@ -0,0 +1,13 @@
+package br.com.matsumura.TDD_Software;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddSoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/matsumura/TDD_Software/TddSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matsumura.TDD_Software;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
