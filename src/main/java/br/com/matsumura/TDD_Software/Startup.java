d0fdc8513d7330e1d8de6f51b8419bdf79c4265b
fatec-dsm6 <fatec@fate.sp.gov.br>
2025-05-16 19:47:14 -0300

    Modificações iniciais

--- src/main/java/br/com/matsumura/TDD_Software/Startup.java
+++ src/main/java/br/com/matsumura/TDD_Software/Startup.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TddSoftwareApplication {
+public class Startup {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TddSoftwareApplication.class, args);
+		SpringApplication.run(Startup.class, args);
 	}
 
 }
