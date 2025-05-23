8e3c0472499ae54d6ec8e74fc768cd3832bdcf00
Ricardo Matsumura <fuzilo@hotmail.com>
2025-05-22 21:55:26 -0300

    Update MathController.java
    Adicionando novas funções

--- src/main/java/br/com/matsumura/TDD_Software/controllers/MathController.java
+++ src/main/java/br/com/matsumura/TDD_Software/controllers/MathController.java
@@ -1,6 +1,7 @@
 package br.com.matsumura.TDD_Software.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,9 +12,25 @@ public class MathController {
         return "Teste";
     }
 
-    @GetMapping("/sum")
-    public Integer soma(){
-        return 1+2;
+    @GetMapping("/sum/{valor1}/{valor2}")
+    public Integer soma(
+            @PathVariable int valor1,
+            @PathVariable int valor2
+    ){
+        return valor1 + valor2;
+    }
+
+    @GetMapping("/divide/{valor1}/{valor2}")
+    public double divide(
+            @PathVariable int valor1,
+            @PathVariable int valor2
+    ){
+        if(valor2 == 0){
+            throw new ArithmeticException("Division by zero");
+        } else
+
+            return (double) valor1 /valor2;
+
     }
 
 }
